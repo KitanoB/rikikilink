eadc6311faadbb43a30731c91b1698dbdd92afe2
KitanoB <140886576+KitanoB@users.noreply.github.com>
2025-06-03 20:57:12 +0200

    f13 - Ensure that linksservice stats with logging
    ✅ The service starts correctly, connects to PostgreSQL and Redis, and exposes /actuator endpoints. Log files are properly mounted and structured using Logback, with both console and file outputs enabled.

--- link-service/src/main/java/fr/rikiki/rlk/link_service/LinkServiceApplication.java
+++ link-service/src/main/java/fr/rikiki/rlk/link_service/LinkServiceApplication.java
@@ -1,13 +1,18 @@
 package fr.rikiki.rlk.link_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LinkServiceApplication {
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(LinkServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LinkServiceApplication.class, args);
+		LOGGER.debug("LinkService Starts...");
 	}
 
 }
