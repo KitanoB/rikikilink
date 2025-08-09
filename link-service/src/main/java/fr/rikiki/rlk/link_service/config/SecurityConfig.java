be2517cb8991fc6de14bf637905bed93e498fe98
KitanoB <kitanob@proton.me>
2025-08-09 15:42:38 +0200

    b40 - import and javadoc

--- link-service/src/main/java/fr/rikiki/rlk/link_service/config/SecurityConfig.java
+++ link-service/src/main/java/fr/rikiki/rlk/link_service/config/SecurityConfig.java
@@ -2,12 +2,8 @@ package fr.rikiki.rlk.link_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Security configuration class for the link service.
@@ -22,12 +18,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs*/**");
     }
-
-
 }
